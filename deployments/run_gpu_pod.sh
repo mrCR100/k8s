c4cf0cb56e0a6b7371172135bae73366bb76920b
chenr <604441296@qq.com>
2025-02-07 10:43:41 +0800

    add a gpu test deployment file

---
+++ deployments/run_gpu_pod.sh
@@ -0,0 +1,22 @@
+cat <<EOF | kubectl apply -f -
+apiVersion: v1
+kind: Pod
+metadata:
+  name: gpu-pod
+spec:
+  restartPolicy: Never
+  containers:
+    - name: gpu-container
+      image: quay.io/centos/centos
+      command: ["/bin/bash", "-c", "sleep 3600"]
+      resources:
+        requests:
+          nvidia.com/gpu: 2
+        limits:
+          nvidia.com/gpu: 2 # requesting 2 GPU
+  runtimeClassName: nvidia
+  tolerations:
+  - key: nvidia.com/gpu
+    operator: Exists
+    effect: NoSchedule
+EOF
